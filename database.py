c9a8a2f83de3b4cacd881545c123c443a2e470bf
wickliff <odoshew@gmail.com>
2026-08-28 13:24:27 +0300

    Update database configuration and gitignore

--- database.py
+++ database.py
@@ -1,23 +1,29 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Odoshe2003@localhost:5432/mining_db"
-engine=create_engine(DATABASE_URL)
+DATABASE_URL = "sqlite:///./Mining.db"
 
-sessionlocal=sessionmaker(
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
-
 )
 
-Base=declarative_base()
+Base = declarative_base()
+
 
 def get_db():
-    db=sessionlocal()
+
+    db = SessionLocal()
+
     try:
         yield db
     finally:
-        db.close    
+        db.close()  
 
         
